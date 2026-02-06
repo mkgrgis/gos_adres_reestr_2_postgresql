544a1b58a456798c4bac7850d16ad4dcaebeec01
Михаил <41448637+mkgrgis@users.noreply.github.com>
2026-02-06 16:30:39 +0300

    Upd tests

--- GitHubActions/db_test.sh
+++ GitHubActions/db_test.sh
@@ -2,15 +2,15 @@ mkdir test/result;
 
 echo "ФАЙЛЫ ГАР
 ----------------------------------------------------------------------------"
-echo "select "№", xsd_id, loading_session_id, tstamp, xsd_filename, xml_file_prefix, table_name, root_node, singular_transport_node, xsd_descr_general, xsd_descr_singular
-from xsd.transport_files;" | psql > test/result/files.out;
-diff test/expected/files.out test/result/files.out > test/files.diff;
+echo 'select "№", xsd_id, loading_session_id, tstamp, xsd_filename, xml_file_prefix, table_name, root_node, singular_transport_node, xsd_descr_general, xsd_descr_singular
+from xsd.transport_files;' | psql > test/result/xml_files.out;
+diff test/expected/xml_files.out test/result/xml_files.out > test/xml_files.diff;
 [ $? -eq 0 ] && echo "Успешно";
 echo "АТРИБУТЫ ГАР
 ----------------------------------------------------------------------------"
-echo "select xsd_id, loading_session_id, tstamp, xsd_filename, root_node, "№", transport_attribute, column_name, xsd_dt, "usage", "not null", "type", "cardinality", length, name0, table_name, xml_file_prefix
-from xsd.transport_attributes;" | psql > test/result/att.out;
-diff test/expected/att.out test/result/att.out > test/att.diff;
+echo 'select xsd_id, loading_session_id, tstamp, xsd_filename, root_node, "№", transport_attribute, column_name, xsd_dt, "usage", "not null", "type", "cardinality", "length", name0, table_name, xml_file_prefix
+from xsd.transport_attributes;' | psql > test/result/xml_att.out;
+diff test/expected/xml_att.out test/result/xml_att.out > test/xml_att.diff;
 [ $? -eq 0 ] && echo "Успешно";
 
 ddf='sql/ГАР_pgDDL.sql';
